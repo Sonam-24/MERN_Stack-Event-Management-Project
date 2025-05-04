c712a6cc1e6885f52a752c6a679f07b1bac556a3
Sonam <sagar.sonam24@gmail.com>
2025-05-04 12:51:20 +0530

    Initial commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+        Welcome to<strong> Majestic Plans</strong> , your trusted partner in
+        crafting unforgettable experiences. We are a full-service event
+        management company dedicated to turning your ideas into reality with
+        precision, creativity, and flair. Whether you're planning a corporate
+        function, a lavish wedding, or a private celebration, we’re here to make
+        every detail shine.
+      </p>
+      <p>
+        {" "}
+        Founded on the belief that every event should be unique, meaningful, and
+        memorable, our team brings passion, professionalism, and expertise to
+        every project. With years of experience in the industry, we specialize
+        in <strong>end-to-end event planning</strong>, from concept development
+        to flawless execution. No matter the scale, we take care of every detail
+        so you can enjoy the moment stress-free. Our services include
+        <strong>
+          {" "}
+          corporate events, weddings, birthday parties, product launches,
+          conferences, award ceremonies, and more
+        </strong>
+        . We provide customized event solutions that reflect your vision, brand,
+        or personal style. From venue selection and theme design to vendor
+        coordination and on-site management, we handle it all with dedication
+        and care. What sets us apart is our commitment to excellence and our
+        ability to adapt to your needs.
+      </p>
+      <p>
+        {" "}
+        We listen closely to our clients, ensuring that every event we produce
+        is a true reflection of their desires and objectives. Our team works
+        with{" "}
+        <strong>
+          top-notch vendors, caterers, decorators, and entertainers
+        </strong>
+        , ensuring every aspect of your event meets the highest standards. We
+        believe in creating not just events, but lasting memories. Our creative
+        designers and planners are always exploring new trends, innovative
+        décor, and engaging entertainment ideas to make your event truly
+        special.
+      </p>
+      <p>
+        Let us handle the stress while you enjoy the spotlight. Whether it's a
+        once-in-a-lifetime celebration or a recurring corporate function,{" "}
+        <strong>Majestic Plans </strong>is here to elevate your event with
+        passion and professionalism. Connect with us today and let’s start
+        planning something extraordinary together.
+      </p>
+    </section>
+  );
+};
+
+export default About;
